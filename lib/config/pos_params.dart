caf184de272652f86bacf0e5df2738cd7c1c18cf
MaalejAymanFMS <maalejfms@gmail.com>
2024-03-06 17:24:20 +0100

    changes in posparamsssv3

--- lib/config/pos_params.dart
+++ lib/config/pos_params.dart
@@ -4,19 +4,19 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class PosParams{
   
-  static late String? ngrokURL="https://prime-verified-pug.ngrok-free.app" ;
-  static late String erpnextURL = "https://erpnext-141144-0.cloudclusters.net";
-  static late String token = "token 1e36fdb116a0d37:cf66f1ad25b58ba";
-  static late String comapny = "Jumpark";
-  static late String productFilter = '';
-  static late String stores = "Stores_JP";
-  static late String cash = "Cash_JP";
-  static late String sales = "Sales_JP";
-  static late String main = "Main_JP";
-  static late String debtors = "Debtors_JP";
-  static late String bank = "BTE_JP";
+  static late String? ngrokURL ;
+  static late String erpnextURL ;
+  static late String token ;
+  static late String comapny ;
+  static late String productFilter ;
+  static late String stores ;
+  static late String cash ;
+  static late String sales ;
+  static late String main ;
+  static late String debtors ;
+  static late String bank ;
   static Future<void> initialize() async {
-    await dotenv.load();
+    await dotenv.load(fileName: "../../.env");
     ngrokURL = dotenv.env['ngrokURL']!;
     erpnextURL = dotenv.env['erpnextURL']!;
     token = dotenv.env['token']!;
